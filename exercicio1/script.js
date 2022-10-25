9d4dffcd2de69941b5568433f2c749a72b35d36a
Rafaela Guedes da Costa <rafaela.costa@meta.com.br>
2022-10-25 19:53:34 -0300

    resolução

---
+++ exercicio1/script.js
@@ -0,0 +1,74 @@
+/*
+//Parte A
+const imprima = (nome) => {
+    return console.log(`Olá, ${nome}!`)
+}
+imprima('Rafa')
+
+//Parte B
+const tabuada = (num) => {
+    console.log(`Tabuada do número ${num}`)
+    r0 = 0 + num
+    console.log(`0 + ${num} = ${r0} `)
+    r1 = 1 + num
+    console.log(`1 + ${num} = ${r1} `)
+    r2 = 2 + num
+    console.log(`2 + ${num} = ${r2} `)
+    r3 = 3 + num
+    console.log(`3 + ${num} = ${r3} `)
+    r4 = 4 + num
+    console.log(`4 + ${num} = ${r4} `)
+    r5 = 5 + num
+    console.log(`5 + ${num} = ${r5} `)
+    r6 = 6 + num
+    console.log(`6 + ${num} = ${r6} `)
+    r7 = 7 + num
+    console.log(`7 + ${num} = ${r7} `)
+    r8 = 8 + num
+    console.log(`8 + ${num} = ${r8} `)
+    r9 = 9 + num
+    console.log(`9 + ${num} = ${r9} `)
+    r10 = 10 + num
+    console.log(`10 + ${num} = ${r10} `)
+}
+
+tabuada(3)
+tabuada(10)
+*/
+
+//Parte A
+function imprima (nome) {
+    return console.log(`Olá, ${nome}!`)
+}
+imprima('Rafa')
+
+//Parte B
+function tabuada (num) {
+    console.log(`Tabuada do número ${num}`)
+    r0 = 0 + num
+    console.log(`0 + ${num} = ${r0} `)
+    r1 = 1 + num
+    console.log(`1 + ${num} = ${r1} `)
+    r2 = 2 + num
+    console.log(`2 + ${num} = ${r2} `)
+    r3 = 3 + num
+    console.log(`3 + ${num} = ${r3} `)
+    r4 = 4 + num
+    console.log(`4 + ${num} = ${r4} `)
+    r5 = 5 + num
+    console.log(`5 + ${num} = ${r5} `)
+    r6 = 6 + num
+    console.log(`6 + ${num} = ${r6} `)
+    r7 = 7 + num
+    console.log(`7 + ${num} = ${r7} `)
+    r8 = 8 + num
+    console.log(`8 + ${num} = ${r8} `)
+    r9 = 9 + num
+    console.log(`9 + ${num} = ${r9} `)
+    r10 = 10 + num
+    console.log(`10 + ${num} = ${r10} `)
+}
+
+tabuada(3)
+tabuada(10)
+
